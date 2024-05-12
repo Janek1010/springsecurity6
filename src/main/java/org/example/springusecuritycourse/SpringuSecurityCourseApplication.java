3891d825020d01c2d2ef2205d2d6bd60ca94776a
Janek1010 <janekdomozych99@gmail.com>
2024-05-12 20:45:03 +0200

    spring security basics

---
+++ src/main/java/org/example/springusecuritycourse/SpringuSecurityCourseApplication.java
@@ -0,0 +1,13 @@
+package org.example.springusecuritycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringuSecurityCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringuSecurityCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springusecuritycourse/SpringuSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springusecuritycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringuSecurityCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
