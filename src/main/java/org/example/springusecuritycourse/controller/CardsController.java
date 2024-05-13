4455044bdbad2335fafc3ef1829b1f7971ee2159
Janek1010 <janekdomozych99@gmail.com>
2024-05-13 15:03:03 +0200

    spring security basics

---
+++ src/main/java/org/example/springusecuritycourse/controller/CardsController.java
@@ -0,0 +1,14 @@
+package org.example.springusecuritycourse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardsController {
+
+    @GetMapping("/myCards")
+    public String getAccountDetails(){
+        return "Here are the card details from the DB";
+    }
+    
+}
--- src/main/java/org/example/springusecuritycourse/controller/ContactController.java
+++ src/main/java/org/example/springusecuritycourse/controller/ContactController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
-    @GetMapping("/welcome")
-    public String sayWelcome(){
-        return "Welcome to Spring Application with Security";
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String getAccountDetails(){
+        return "Inquiry details are saved to the DB";
     }
+    
 }
